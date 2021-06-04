07fc6d4911c867dd9c8933af4b6484191515cc20
unknown <siddhigate16@gmail.com>
2021-06-04 19:10:19 +0530

    added backend

---
+++ backend/README.md
@@ -0,0 +1,16 @@
+# E-commerce backend
+
+ E-commerce backend built using NodeJS, ExpressJS and MongoDB
+
+# 
+![mindmap](./mindmap.png)
+
+ ## Functionalities
+1.  User authentication
+2.  Admin authentication
+3.  Product CRUD 
+4.  Category CRUD
+5.  Order CRUD
+6.  Managing user orders
+
+#
---
+++ backend/app.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose');;
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cors = require('cors'); 
+
+// my routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const orderRoutes = require("./routes/order");
+
+// db connection
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("CONNECTED TO DATABASE");
+}).catch(() => {
+    console.log("Oops CAN'T CONNECT TO DATABASE");
+});
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(cors());
+
+// Routes
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api",categoryRoutes);
+app.use("/api",productRoutes);
+app.use("/api", orderRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+
+// starting server
+app.listen(port, ()=>{
+    console.log(`App is running at ${port}`);
+})
